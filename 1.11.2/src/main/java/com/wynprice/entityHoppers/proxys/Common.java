dbfc2b902ec9aac371ae4094393eb161204a4bb5
Wyn Price <wynprice999@gmail.com>
2017-06-14 21:44:39 +0100

    initial commit

---
+++ 1.11.2/src/main/java/com/wynprice/entityHoppers/proxys/Common.java
@@ -0,0 +1,27 @@
+package com.wynprice.entityHoppers.proxys;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class Common {
+
+	public void PreInit(FMLPreInitializationEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Init(FMLInitializationEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void PostInit(FMLPostInitializationEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
