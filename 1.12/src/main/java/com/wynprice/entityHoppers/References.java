267cd3307d69f55357fa11af8487d6606a527edc
Wyn Price <wynprice999@gmail.com>
2017-06-16 20:33:23 +0100

    updated versions

--- 1.12/src/main/java/com/wynprice/entityHoppers/References.java
+++ 1.12/src/main/java/com/wynprice/entityHoppers/References.java
@@ -4,7 +4,7 @@ public class References
 {
 	public static final String MODID = "entity_hoppers";
 	public static final String NAME = "Entity Hoppers";
-	public static final String VERSION = "0.1.0-1.12";
+	public static final String VERSION = "0.2.0-1.12";
 	public static final String CLIENT_PROXY = "com.wynprice.entityHoppers.proxys.Client";
 	public static final String SERVER_PROXY = "com.wynprice.entityHoppers.proxys.Server";
 	public static final String UPDATE_URL = "http://www.wynprice.com/moddedUpdateCheckers/entityHoppers.json";
